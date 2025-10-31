a65da152a1ef5b17a39247a1ec27fde3f8e2024e
SungSoo Yoo <elcane2@ajou.ac.kr>
2025-10-31 19:31:55 +0900

    1 to 5

---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,21 @@
+/**
+ * Naming Conventions
+ * 
+ * 1)일반적으로 영어를 사용하며 문자와 숫자 모두 사용 가능
+ * 2) 언더스코어와 달러를 제외한 특수기호는 X
+ * 3) 숫자로 이름을 시작 X
+ * 4) 키워드는 변수명으로 사용 불가
+ * 
+ */
+
+let codefac = '코드팩토리';
+var $ive = '아이브';
+const _yuJin = '안유진';
+
+/** 
+ * Naming Convention 2
+ * 
+ * 1) camelCase -> 대부분의 언어에서 사용
+ * 2) snake_case;
+ * 3)PascalCase -> C# 등에서 사용
+ */
